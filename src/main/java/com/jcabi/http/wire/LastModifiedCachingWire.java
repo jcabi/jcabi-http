9ad814466e0f5bcfecda1b89758c344b4b083e4e
Igor Piddubnyi <igor.piddubnyi@solvians.com>
2016-01-11 18:37:19 +0100

    #90 Remove package field

--- src/main/java/com/jcabi/http/wire/LastModifiedCachingWire.java
+++ src/main/java/com/jcabi/http/wire/LastModifiedCachingWire.java
@@ -52,7 +52,7 @@ public final class LastModifiedCachingWire implements Wire {
     /**
      * Last-Modified header name.
      */
-    static final String LAST_MODIFIED = "Last-Modified";
+    public static final String LAST_MODIFIED = "Last-Modified";
 
     /**
      * If-Modified-Since header name.
