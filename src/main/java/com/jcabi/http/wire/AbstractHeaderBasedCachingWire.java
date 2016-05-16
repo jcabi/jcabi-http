c552ca3cdc9c5e9d80683b30129a436fced84f7f
Alan Evans <alan.evans@burrows.yr.com>
2016-05-16 13:55:14 +0100

    Solve #120 Evict cache entry if If-Modified-Since request responded

--- src/main/java/com/jcabi/http/wire/AbstractHeaderBasedCachingWire.java
+++ src/main/java/com/jcabi/http/wire/AbstractHeaderBasedCachingWire.java
@@ -138,8 +138,7 @@ public abstract class AbstractHeaderBasedCachingWire implements Wire {
     }
 
     /**
-     * Check response and update cache if needed.
-     * @todo #90:30min Evict cache entry if If-Modified-Since request responded
+     * Check response and update cache or evict from cache if needed.
      *  with HTTP_OK code and no Last-Modified header.
      * @param req Request
      * @param home URI to fetch
@@ -173,13 +172,17 @@ public abstract class AbstractHeaderBasedCachingWire implements Wire {
     }
 
     /**
-     * Add response to cache.
+     * Add response to cache,
+     * Update response in cache or
+     * Evict response from cache.
      * @param req The request to be used as key
      * @param rsp The response to add
      */
     private void updateCache(final Request req, final Response rsp) {
         if (rsp.headers().containsKey(this.scvh)) {
             this.cache.put(req, rsp);
+        } else {
+            this.cache.remove(req);
         }
     }
 
