7d08ffecb023c144849263e05f3ef66933e61232
Хлебников Андрей <viruszold@gmail.com>
2017-03-18 03:22:55 +0300

    Formatting

--- src/main/java/com/jcabi/http/response/YmlResponse.java
+++ src/main/java/com/jcabi/http/response/YmlResponse.java
@@ -1,3 +1,32 @@
+/**
+ * Copyright (c) 2011-2017, jcabi.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: 1) Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer. 2) Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3) Neither the name of the jcabi.com nor
+ * the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.jcabi.http.response;
 
 import com.jcabi.aspects.Immutable;
@@ -8,7 +37,6 @@ import org.yaml.snakeyaml.Yaml;
 
 /**
  * Yaml response.
- *
  *
  * <pre>static final class Configuration {
  *     public String name;
@@ -22,7 +50,7 @@ import org.yaml.snakeyaml.Yaml;
  *
  * <p>The class is immutable and thread-safe.
  *
- * @author Pastor (viruszold@gmail.com)
+ * @author Khlebnikov Andrey (viruszold@gmail.com)
  * @version $Id$
  * @see <a href="www.snakeyaml.org/">SnakeYaml website</a>
  */
@@ -31,11 +59,20 @@ import org.yaml.snakeyaml.Yaml;
 public final class YmlResponse extends AbstractResponse {
     private final Yaml yaml = new Yaml();
 
-    public YmlResponse(Response resp) {
+    /**
+     * Public ctor.
+     * @param resp Response
+     */
+    public YmlResponse(final Response resp) {
         super(resp);
     }
 
-    public <E> E loadAs(Class<? extends E> clazz) {
-        return yaml.loadAs(super.body(), clazz);
+    /**
+     *
+     * @param clazz Deserialized class
+     * @return Deserialized object
+     */
+    public <E> E loadAs(final Class<? extends E> clazz) {
+        return this.yaml.loadAs(super.body(), clazz);
     }
 }
