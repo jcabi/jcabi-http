16d169c0ab3030916960c68002ceee342a4d9177
andreoss <andreoss@sdf.org>
2021-06-27 02:23:40 -0400

    (#254) Introduce integration tests based on HttpBin

--- src/test/java/com/jcabi/http/RequestSecondITCase.java
+++ src/test/java/com/jcabi/http/RequestSecondITCase.java
@@ -31,37 +31,75 @@ package com.jcabi.http;
 
 import com.jcabi.http.request.ApacheRequest;
 import com.jcabi.http.request.JdkRequest;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.net.URI;
-import lombok.SneakyThrows;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.TestInstance;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 /**
- * Template for generic tests for {@link Request}.
+ * Integration test for {@link Request}.
  *
- * @since 1.17.4
+ * @since 1.17.8
  */
 @SuppressWarnings("PMD.AbstractClassWithoutAbstractMethod")
-abstract class RequestTestTemplate {
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Testcontainers(disabledWithoutDocker = true)
+public final class RequestSecondITCase {
+
     /**
-     * Annotation for a parameterized test case.
-     *
-     * @since 1.17.4
+     * Container with HttpBin.
      */
-    @Retention(RetentionPolicy.RUNTIME)
-    @ValueSource(classes = {ApacheRequest.class, JdkRequest.class})
-    protected @interface Values {
+    private final GenericContainer<?> container = new GenericContainer<>(
+        DockerImageName.parse("kennethreitz/httpbin")
+    ).withExposedPorts(80);
+
+    @BeforeAll
+    void beforeAll() {
+        this.container.start();
+    }
+
+    @AfterAll
+    void tearDown() {
+        this.container.stop();
     }
 
     /**
-     * Make a request.
-     * @param uri URI to start with
-     * @param type Type of the request
-     * @return Request
+     * URI of the container.
+     * @return URI.
      */
-    @SneakyThrows
-    static Request request(final URI uri, final Class<? extends Request> type) {
-        return type.getDeclaredConstructor(URI.class).newInstance(uri);
+    private URI uri() {
+        return URI.create(
+            String.format(
+                "http://%s:%d",
+                this.container.getHost(),
+                this.container.getFirstMappedPort()
+            )
+        );
+    }
+
+    /**
+     * Test for {@link JdkRequest}.
+     * @since 1.17.8
+     */
+    @Nested
+    final class JdkRequestITCase extends RequestITCaseTemplate {
+        JdkRequestITCase() {
+            super(JdkRequest.class, RequestSecondITCase.this.uri());
+        }
+    }
+
+    /**
+     * Test for {@link ApacheRequest}.
+     * @since 1.17.8
+     */
+    @Nested
+    final class ApacheRequestITCase extends RequestITCaseTemplate {
+        ApacheRequestITCase() {
+            super(ApacheRequest.class, RequestSecondITCase.this.uri());
+        }
     }
 }
