7b16fd3698d407ab995cb9b13c73997d834523a7
uchitsa <uchitsa@ya.ru>
2024-12-13 01:08:45 +0300

    fix quality violations

--- src/test/java/com/jcabi/http/response/RestResponseITCase.java
+++ src/test/java/com/jcabi/http/response/RestResponseITCase.java
@@ -65,7 +65,7 @@ import org.junit.jupiter.api.function.Executable;
                 @Override
                 public void execute() {
                     MatcherAssert.assertThat(
-                        "should contains value 'abc",
+                        "should contains value 'abc'",
                         resp.cookie("xyz"),
                         Matchers.hasProperty("value", Matchers.is("abc"))
                     );
@@ -77,7 +77,7 @@ import org.junit.jupiter.api.function.Executable;
     @Test
     void readsCookies() throws IOException {
         MatcherAssert.assertThat(
-            "should contains value 'bar",
+            "should contains value 'bar'",
             new JdkRequest("https://httpbin.org/cookies/set?foo=bar")
                 .fetch()
                 .as(RestResponse.class)
