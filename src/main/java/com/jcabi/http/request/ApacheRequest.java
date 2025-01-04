5671e17db0b3a76c1086c9edcbf96b0cd7f79c96
uchitsa <uchitsa@ya.ru>
2025-01-04 03:00:40 +0300

    fix quality violations

--- src/main/java/com/jcabi/http/request/ApacheRequest.java
+++ src/main/java/com/jcabi/http/request/ApacheRequest.java
@@ -89,14 +89,12 @@ public final class ApacheRequest implements Request {
             final InputStream content,
             final int connect,
             final int read) throws IOException {
-            final CloseableHttpResponse response =
-                HttpClients.createSystem().execute(
-                    this.httpRequest(
-                        home, method, headers, content,
-                        connect, read
-                    )
-                );
-            try {
+            try (CloseableHttpResponse response = HttpClients.createSystem().execute(
+                this.httpRequest(
+                    home, method, headers, content,
+                    connect, read
+                )
+            )) {
                 return new DefaultResponse(
                     req,
                     response.getStatusLine().getStatusCode(),
@@ -104,8 +102,6 @@ public final class ApacheRequest implements Request {
                     this.headers(response.getAllHeaders()),
                     this.consume(response.getEntity())
                 );
-            } finally {
-                response.close();
             }
         }
 
